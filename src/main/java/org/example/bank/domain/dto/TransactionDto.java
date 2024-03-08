68eb455720b6be3704ad9ed59d0ecfb24f76e7fb
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 21:06:36 -0500

    The TransactionDto class was created

--- src/main/java/org/example/bank/domain/dto/TransactionDto.java
+++ src/main/java/org/example/bank/domain/dto/TransactionDto.java
@@ -1,4 +1,20 @@
 package org.example.bank.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.bank.domain.valueObjects.TypeTransaction;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionDto {
+
+    private String id;
+    private Date dateTransaction;
+    private TypeTransaction typeTransaction;
+    private String sourceAccount;
+    private String destinationAccount;
 }
