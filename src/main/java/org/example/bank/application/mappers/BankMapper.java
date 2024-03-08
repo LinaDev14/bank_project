9aa8162a449a8b143d1b921172da7a1b07d434bf
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 21:27:38 -0500

    The BackMapper was implemented

--- src/main/java/org/example/bank/application/mappers/BankMapper.java
+++ src/main/java/org/example/bank/application/mappers/BankMapper.java
@@ -1,7 +1,35 @@
 package org.example.bank.application.mappers;
 
+import org.example.bank.domain.collections.Bank;
+import org.example.bank.domain.dto.BankDto;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
 
 @Component
 public class BankMapper {
+
+    public Function<Bank, BankDto> mapToDtoBank(){
+
+        return bank -> new BankDto(
+                bank.getId(),
+                bank.getName(),
+                bank.getPhone(),
+                bank.getCountry(),
+                bank.getBranchOffice(),
+                bank.getIdCustomer()
+        );
+    }
+
+    public Function<BankDto, Bank> mapToCollectionBank(){
+
+        return bankDto -> new Bank(
+                bankDto.getId(),
+                bankDto.getName(),
+                bankDto.getPhone(),
+                bankDto.getCountry(),
+                bankDto.getBranchOffice(),
+                bankDto.getIdCustomer()
+        );
+    }
 }
