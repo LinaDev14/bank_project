43ee1b4edec38617495c3dca0f25a2f67561526b
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 21:00:34 -0500

    The transaction class was created

--- src/main/java/org/example/bank/domain/collections/Transaction.java
+++ src/main/java/org/example/bank/domain/collections/Transaction.java
@@ -1,4 +1,21 @@
 package org.example.bank.domain.collections;
 
+import lombok.Data;
+import org.example.bank.domain.valueObjects.TypeTransaction;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Data
+@Document(value = "Transactions")
 public class Transaction {
+
+    @Id
+    private final String id;
+    private final Date dateTransaction;
+    private final TypeTransaction typeTransaction;
+    private final String sourceAccount;
+    private final String destinationAccount;
+
 }
---
+++ src/main/java/org/example/bank/domain/valueObjects/TypeTransaction.java
@@ -0,0 +1,10 @@
+package org.example.bank.domain.valueObjects;
+
+public enum TypeTransaction {
+
+    TRANSFER,
+    DEPOSIT,
+    CARD_PAYMENT,
+    ONLINE_PURCHASE,
+    WITHDRAW
+}
