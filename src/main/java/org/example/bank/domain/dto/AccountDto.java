deb98ad96e1a16ca2871e4e349adb4f3872b4a23
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 21:03:18 -0500

    The AccountDto class was created

--- src/main/java/org/example/bank/domain/dto/AccountDto.java
+++ src/main/java/org/example/bank/domain/dto/AccountDto.java
@@ -1,13 +1,24 @@
 package org.example.bank.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.example.bank.domain.valueObjects.Status;
 import org.example.bank.domain.valueObjects.TypeAccount;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccountDto {
 
-
+    private String id;
+    private Status status;
+    private String dateCreation;
+    private TypeAccount typeAccount;
+    private String numberAccount;
+    private Integer balance;
+    private Integer discount;
+    private Integer discountedBalance;
 }
--- src/main/java/org/example/bank/domain/repository/AccountRepository.java
+++ src/main/java/org/example/bank/domain/repository/AccountRepository.java
@@ -1,4 +1,13 @@
 package org.example.bank.domain.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.bank.domain.valueObjects.Status;
+import org.example.bank.domain.valueObjects.TypeAccount;
+
+
 public interface AccountRepository {
+
+
 }
