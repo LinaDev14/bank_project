7e3128dec5e83a2e9b012bbf8481254f02041ff3
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 22:52:35 -0500

    The interfaces were implemented

--- src/main/java/org/example/bank/application/service/Transaction/TransactionInterface.java
+++ src/main/java/org/example/bank/application/service/Transaction/TransactionInterface.java
@@ -1,4 +1,22 @@
 package org.example.bank.application.service.Transaction;
 
+import org.example.bank.domain.dto.TransactionDto;
+import org.example.bank.domain.valueObjects.TypeTransaction;
+
+import java.util.List;
+
 public interface TransactionInterface {
+
+    // findById
+    TransactionDto findById(String id);
+
+    // save all data
+    List<TransactionDto> saveAllData(List<TransactionDto> transactionDto);
+
+    // save transaction
+    TransactionDto saveTransaction(TransactionDto transactionDto);
+
+    // list type transaction
+    List<TransactionDto> findByTypeTransaction(TypeTransaction typeTransaction);
+
 }
--- src/main/java/org/example/bank/application/service/Transaction/TransactionService.java
+++ src/main/java/org/example/bank/application/service/Transaction/TransactionService.java
@@ -1,4 +1,28 @@
 package org.example.bank.application.service.Transaction;
 
-public class TransactionService {
+import org.example.bank.domain.dto.TransactionDto;
+import org.example.bank.domain.valueObjects.TypeTransaction;
+
+import java.util.List;
+
+public class TransactionService implements TransactionInterface{
+    @Override
+    public TransactionDto findById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<TransactionDto> saveAllData(List<TransactionDto> transactionDto) {
+        return null;
+    }
+
+    @Override
+    public TransactionDto saveTransaction(TransactionDto transactionDto) {
+        return null;
+    }
+
+    @Override
+    public List<TransactionDto> findByTypeTransaction(TypeTransaction typeTransaction) {
+        return null;
+    }
 }
