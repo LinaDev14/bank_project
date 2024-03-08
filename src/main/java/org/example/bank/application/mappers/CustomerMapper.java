b28ab1d70cd0882bab3a0e0cd5f92a174a6bf7cf
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 21:32:43 -0500

    The CustomerMapper was implemented

--- src/main/java/org/example/bank/application/mappers/CustomerMapper.java
+++ src/main/java/org/example/bank/application/mappers/CustomerMapper.java
@@ -1,7 +1,45 @@
 package org.example.bank.application.mappers;
 
+import org.example.bank.domain.collections.Customer;
+import org.example.bank.domain.dto.CustomerDto;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
 
 @Component
 public class CustomerMapper {
+
+    public Function<Customer, CustomerDto> mapToDtoCustomer(){
+
+        return customer -> new CustomerDto(
+                customer.getId(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getPhone(),
+                customer.getEmail(),
+                customer.getAddress(),
+                customer.getAge(),
+                customer.getTypeIdentification(),
+                customer.getNumberIdentification(),
+                customer.getTypePerson(),
+                customer.getIdAccount()
+        );
+    }
+    
+    public Function<CustomerDto, Customer> mapToCollectionCustomer(){
+
+        return customerDto -> new Customer(
+                customerDto.getId(),
+                customerDto.getFirstName(),
+                customerDto.getLastName(),
+                customerDto.getPhone(),
+                customerDto.getEmail(),
+                customerDto.getAddress(),
+                customerDto.getAge(),
+                customerDto.getTypeIdentification(),
+                customerDto.getNumberIdentification(),
+                customerDto.getTypePerson(),
+                customerDto.getIdAccount()
+        );
+    }
 }
