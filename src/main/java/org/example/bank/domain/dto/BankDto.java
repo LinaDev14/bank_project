980cfac0fba235f3231caaa42661fb6365754ece
LinaDev14 <perfilgitlinamaria@gmail.com>
2024-03-07 21:05:32 -0500

    The CustomerDto class was created

--- src/main/java/org/example/bank/domain/dto/BankDto.java
+++ src/main/java/org/example/bank/domain/dto/BankDto.java
@@ -1,4 +1,21 @@
 package org.example.bank.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.bank.domain.valueObjects.BranchOffice;
+import org.example.bank.domain.valueObjects.Country;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BankDto {
+
+    private String id;
+    private String name;
+    private String phone;
+    private Country country;
+    private BranchOffice branchOffice;
+    private String idCustomer;
 }
--- src/main/java/org/example/bank/domain/dto/CustomerDto.java
+++ src/main/java/org/example/bank/domain/dto/CustomerDto.java
@@ -1,4 +1,27 @@
 package org.example.bank.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.bank.domain.valueObjects.TypeIdentification;
+import org.example.bank.domain.valueObjects.TypePerson;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDto {
+
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String address;
+    private String age;
+    private TypeIdentification typeIdentification;
+    private String numberIdentification;
+    private TypePerson typePerson;
+    private String IdAccount;
 }
